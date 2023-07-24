3d7b6242946eb6c39870327b2242bc85160b6109
İkbal Avşar <ikbal.avsar@one.com.tr>
2023-07-24 10:38:04 +0300

    MoveZeroes

---
+++ LeetCodeAlgorithms/LeetCodeAlgorithms/MoveZeroes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeAlgorithms
+{
+    class MoveZeroes
+    {
+        public void MoveZeroess(int[] nums)
+        {
+            int nonZeroIndex = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    nums[nonZeroIndex] = nums[i];
+                    nonZeroIndex++;
+                }
+            }
+
+
+            while (nonZeroIndex < nums.Length)
+            {
+                nums[nonZeroIndex] = 0;
+                nonZeroIndex++;
+            }
+
+        }
+    }
+}
