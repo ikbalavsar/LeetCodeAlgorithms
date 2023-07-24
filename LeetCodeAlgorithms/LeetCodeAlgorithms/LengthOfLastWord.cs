309cb8b06a25bd4e1de76528372a282ced51fc99
İkbal Avşar <ikbal.avsar@one.com.tr>
2023-07-24 14:38:33 +0300

    LengthOfLastWord

---
+++ LeetCodeAlgorithms/LeetCodeAlgorithms/LengthOfLastWord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeAlgorithms
+{
+    class LengthOfLastWord
+    {
+        public static int LengthOfLastWordd(string s)
+        {
+            string[] words = s.Split(" ");
+            words = words.Where(str => !string.IsNullOrEmpty(str)).ToArray();
+
+
+            int length = words.Length;
+
+            var lengthOfLastWord = words[length - 1].Length;
+
+            return lengthOfLastWord;
+
+        }
+    }
+}
--- LeetCodeAlgorithms/LeetCodeAlgorithms/Program.cs
+++ LeetCodeAlgorithms/LeetCodeAlgorithms/Program.cs
@@ -3,10 +3,8 @@ using LeetCodeAlgorithms;
 
 Console.WriteLine("Hello, World!");
 
-var s = "abc";
-var t = "ahbgdc";
 
-bool result = IsSubsequence.IsSubsequencee(s, t);
-Console.WriteLine(result);
+
+
 
 
