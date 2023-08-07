e68571ce74d7874f83a2127680b63d9872e85b28
İkbal Avşar <ikbal.avsar@one.com.tr>
2023-08-07 16:49:27 +0300

    Hackerrank-1

---
+++ LeetCodeAlgorithms/LeetCodeAlgorithms/PlusMinus.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeAlgorithms
+{
+     class PlusMinus
+    {
+
+        /*
+         * Complete the 'plusMinus' function below.
+         *
+         * The function accepts INTEGER_ARRAY arr as parameter.
+         */
+
+        public static void plusMinus(List<int> arr)
+        {
+
+            var positiveCount = 0;
+            var negativeCount = 0;
+            var zeroCount = 0;
+
+            foreach (int item in arr)
+            {
+                if (item > 0)
+                {
+                    positiveCount++;
+                }
+                else if (item < 0)
+                {
+                    negativeCount++;
+                }
+                else
+                {
+                    zeroCount++;
+                }
+            }
+
+            List<double> newArr = new List<double>();
+
+            double positiveRatio = (double)positiveCount / arr.Count();
+            double negativeRatio = (double)negativeCount / arr.Count();
+            double zeroRatio = (double)zeroCount / arr.Count();
+
+
+            Console.WriteLine(positiveRatio);
+            Console.WriteLine(negativeRatio);
+            Console.WriteLine(zeroRatio);
+
+
+        }
+    }
+}
