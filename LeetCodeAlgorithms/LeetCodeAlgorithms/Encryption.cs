c5281e448a2d22c23e8fd2c1f06e22d366b00fa3
İkbal Avşar <ikbal.avsar@one.com.tr>
2023-08-14 12:43:03 +0300

    Encryption

---
+++ LeetCodeAlgorithms/LeetCodeAlgorithms/Encryption.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeAlgorithms
+{
+    public class Encryption
+    {
+        //        Input(stdin) haveaniceday
+        //
+        //        Expected Output hae and via ecy
+        //
+        public static string encryption(string s)
+        {
+            var strCount = s.Count();
+
+            double squareRoot = Math.Sqrt(strCount);
+
+            var upperLimit = (int)Math.Ceiling(squareRoot);
+            var downLimit = (int)Math.Floor(squareRoot);
+
+            if (upperLimit * downLimit < strCount)
+            {
+                downLimit++;
+            }
+
+            string[] newStrList = new string[downLimit];
+            string resultString = "";
+            var j = 0;
+
+            for (int i = 0; i < strCount; i += upperLimit)
+            {
+
+                string part = s.Substring(i, Math.Min(upperLimit, s.Length - i));
+                newStrList[j] = part;
+                j++;
+
+            }
+
+            for (int i = 0; i < upperLimit; i++)
+            {
+                foreach (var item in newStrList)
+                {
+                    if (i >= item.Length)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        resultString += item[i];
+
+                    }
+                }
+                resultString += " ";
+            }
+            return resultString;
+
+
+
+        }
+
+    }
+}
+}
